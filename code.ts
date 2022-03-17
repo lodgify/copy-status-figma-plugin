c33e8a881a1d5b4d5e8eb739b8b0c35b5dfaf50c
huvber <huvber@gmail.com>
2022-03-17 18:59:56 +0100

    feat: send a slack message

--- code.ts
+++ code.ts
@@ -5,7 +5,15 @@ const WritingStatus = {
   Done: "done",
 };
 
-figma.showUI(__html__, {width: 300, height: 260})
+figma.showUI(__html__, { width: 300, height: 260 });
+const getBody = (text) =>
+  Uint8Array.from(text.split("").map((letter) => letter.charCodeAt(0)));
+
+const getNodeLink = (text) => {
+  const ids = text.id.split(";");
+  const shareId = (ids[1] || ids[0]).replace("I", "");
+  return `https://www.figma.com/file/${figma.fileKey}/?node-id=${shareId}`;
+};
 
 const changeName = (name, status = WritingStatus.Todo) =>
   ({
@@ -30,6 +38,21 @@ figma.ui.onmessage = (msg) => {
     }
     text.setPluginData("status", msg.status);
     text.name = changeName(original, msg.status);
+    if (msg.status === WritingStatus.Todo) {
+      console.log(text, figma.currentPage);
+      //@ts-ignore
+      fetch(
+        "https://hooks.slack.com/workflows/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",
+        {
+          method: "POST",
+          body: getBody(
+            JSON.stringify({
+              frame_link: getNodeLink(text),
+            })
+          ),
+        }
+      );
+    }
   });
   figma.notify(`change ${texts.length} node statuses to ${msg.status}`, {
     timeout: 1500,
